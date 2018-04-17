c2dd3ed4e7d1a6f115a138cff9e1cd16a8b3c6a1
pradhan1234 <=>
2018-04-16 21:44:11 -0500

    added images and db controler will work on managing wishlist

--- php/dbcontroller.php
+++ php/dbcontroller.php
@@ -2,7 +2,7 @@
 class DBController {
 	private $host = "localhost";
 	private $user = "root";
-	private $password = "";
+	private $password = "root";
 	private $database = "craigslist";
 	private $conn;
 	
--- php/wishlist.php
+++ php/wishlist.php
@@ -2,51 +2,62 @@
 session_start();
 require_once("dbcontroller.php");
 $db_handle = new DBController();
+
 if(!empty($_GET["action"])) {
-switch($_GET["action"]) {
-	case "add":
-		// if(!empty($_POST["quantity"])) {
-			$UtilityById = $db_handle->runQuery("SELECT * FROM utility WHERE utility_id='" . $_GET["utility_id"] . "'");
-			$UtilityArray = array($UtilityById[0]["utility_id"]=>array('name'=>$UtilityById[0]["name"], 'utility_id'=>$UtilityById[0]["utility_id"], 'price'=>$UtilityById[0]["price"]));
-			
-			if(!empty($_SESSION["wish_list_item"])) {
-				if(in_array($UtilityById[0]["utility_id"],array_keys($_SESSION["wish_list_item"]))) {
-					foreach($_SESSION["wish_list_item"] as $k => $v) {
-							if($UtilityById[0]["utility_id"] == $k) {
-								if(empty($_SESSION["wish_list_item"][$k]["quantity"])) {
-									$_SESSION["wish_list_item"][$k]["quantity"] = 0;
-								}
-								// $_SESSION["wish_list_item"][$k]["quantity"] += $_POST["quantity"];
-							}
+	switch($_GET["action"]) {
+
+		case "add":
+			// if(!empty($_POST["quantity"])) {
+				$UtilityById = $db_handle->runQuery("SELECT * FROM utility WHERE utility_id='" . $_GET["utility_id"] . "'");
+				$UtilityArray = array($UtilityById[0]["utility_id"]=>array('name'=>$UtilityById[0]["name"], 'utility_id'=>$UtilityById[0]["utility_id"], 'price'=>$UtilityById[0]["price"]));
+				print(UtilityArray);
+				
+				if(!empty($_SESSION["wish_list_item"])) {
+					if(in_array($UtilityById[0]["utility_id"],array_keys($_SESSION["wish_list_item"]))) {
+						// foreach($_SESSION["wish_list_item"] as $k => $v) {
+						// 		if($UtilityById[0]["utility_id"] == $k) {
+						// 			if(empty($_SESSION["wish_list_item"][$k])) {
+						// 				$_SESSION["wish_list_item"][$k]= 0;
+						// 			}
+						// 			// $_SESSION["wish_list_item"][$k]["quantity"] += $_POST["quantity"];
+						// 		}
+						// }
+					} else {
+						echo "<pre>";
+						print_r($UtilityById[0]['utility_id']);
+						print_r(array_keys($_SESSION['wish_list_item']));
+						echo "</pre>";
+
+						$_SESSION["wish_list_item"] = array_merge($_SESSION["wish_list_item"],$UtilityArray);
+
 					}
 				} else {
-					$_SESSION["wish_list_item"] = array_merge($_SESSION["wish_list_item"],$itemArray);
+					$_SESSION["wish_list_item"] = $UtilityArray;
 				}
-			} else {
-				$_SESSION["wish_list_item"] = $UtilityArray;
+			break;
+
+		case "remove":
+			if(!empty($_SESSION["wish_list_item"])) {
+				foreach($_SESSION["wish_list_item"] as $k => $v) {
+						if($_GET["utility_id"] == $k)
+							unset($_SESSION["wish_list_item"][$k]);				
+						if(empty($_SESSION["wish_list_item"]))
+							unset($_SESSION["wish_list_item"]);
+				}
 			}
-		
-	break;
-	case "remove":
-		if(!empty($_SESSION["wish_list_item"])) {
-			foreach($_SESSION["wish_list_item"] as $k => $v) {
-					if($_GET["utility_id"] == $k)
-						unset($_SESSION["wish_list_item"][$k]);				
-					if(empty($_SESSION["wish_list_item"]))
-						unset($_SESSION["wish_list_item"]);
-			}
-		}
-	break;
-	case "empty":
-		unset($_SESSION["wish_list_item"]);
-	break;	
-}
+			break;
+
+		case "empty":
+
+			unset($_SESSION["wish_list_item"]);
+			break;	
+	}
 }
 ?>
 <HTML>
 <HEAD>
 <TITLE></TITLE>
-<link href="style.css" type="text/css" rel="stylesheet" />
+<link href="../css/cart.css" type="text/css" rel="stylesheet" />
 </HEAD>
 <BODY>
 <div id="shopping-cart">
@@ -99,18 +110,24 @@ if(isset($_SESSION["wish_list_item"])){
 		foreach($utility_array as $key=>$value){
 		  $utility_image_array= $db_handle->runQuery("SELECT image_path FROM utility_image where utility_id=".$utility_array[$key]["utility_id"]);
 		  
+		// echo '<br><pre>'; 
+		//   print_r($utility_image_array);
+		//   print_r($utility_image_array[0]['image_path']);
+		// echo '</pre>';
 		?>
 		 <div class="product-item">
-			<form method="post" action="wishlist.php?action=add&utility_id=<?php echo $utility_array[$key]["utility_id"]; ?>">
-			<div class="product-image"><img src="<?php echo $utility_image_array[$key]["image_path"]; ?>"></div>
-			<div><strong><?php echo $utility_array[$key]["name"]; ?></strong></div>
+			<form method="post" action="wishlist.php?action=add&utility_id=<?php echo $utility_array[$key]['utility_id']; ?>">
+			<div class="product-image"><img height="100" width="120" src="<?php echo $utility_image_array[0]['image_path']; ?>"></div>
+			<div><strong><?php echo 
+			$utility_array[$key]["name"]; ?></strong></div>
 			<div class="product-price"><?php echo "$".$utility_array[$key]["price"]; ?></div>
 			<div><input type="submit" value="Add to wishlist" class="btnAddAction" /></div>
+			<br>
 			</form>
-		</div> 
+		</div>
 	
 	<?php
-			}
+		}
 	}
 	?>
 </div>
