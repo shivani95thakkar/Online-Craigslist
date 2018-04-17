52d70fc644a6b703afecbe13ea8829d36f991312
pradhan1234 <=>
2018-04-17 01:50:16 -0500

    integrated product catalog on homepage ang pagination done

--- html/homepage.php
+++ html/homepage.php
@@ -1,8 +1,25 @@
 <?php
-    session_start();
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
     if(!isset($_SESSION['email_id'])){
       header("Location: ../html/login.html");
     }
+
+     $servername = "localhost";
+      $username = "root";
+      $password = "root";
+      $dbname = "craigslist";
+      $limit = 20;
+
+      // Create connection
+      $conn = mysqli_connect($servername, $username, $password, $dbname);
+      
+      // Check connection
+      if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+      }
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,6 +29,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+    <link href="../css/cart.css" type="text/css" rel="stylesheet" />
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -47,8 +65,65 @@
     ?>
 
 
-    <?php 
-      include("../php/product-catalog.php");
-    ?>
+  <div id="product-grid"><!-- 
+  <div class="txt-heading">Utilities</div> -->
+  <?php
+
+  if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };  
+    $start_from = ($page-1) * $limit; 
+
+
+  // $utility_array = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM utility ORDER BY utility_id LIMIT $start_from, $limit;"));
+  $utility_array = array();
+  $result = mysqli_query($conn, "SELECT * FROM utility ORDER BY utility_id LIMIT $start_from, $limit;");
+
+  while($row = mysqli_fetch_assoc($result)){
+    // array_push($utility_array, $row);
+    $img_res =  mysqli_query($conn, "SELECT image_path FROM utility_image where utility_id=".$row["utility_id"]);
+    $path = mysqli_fetch_assoc($img_res);
+
+    // echo "<pre>";
+    // print_r($row);
+    // print_r($path);
+    // echo "</pre>";
+?>
+    <div class="product-item">
+      <form method="post" action="wishlist.php?action=add&utility_id=<?php echo $row["utility_id"]; ?>">
+      <div class="product-image"><img height="100" width="120" src="../php/<?php echo $path["image_path"]; ?>"></div>
+      <div><strong><?php echo 
+      $row["name"]; ?></strong></div>
+      <div class="product-price"><?php echo "$".$row["price"]; ?></div>
+      <div><input type="submit" value="Add to wishlist" class="btnAddAction" /></div>
+      <br>
+      </form>
+    </div>
+<?php } ?>
+
+
+
+</div>
+
+<?php  
+  $sql = "SELECT COUNT(*) FROM utility;";  
+  $rs_result = mysqli_query($conn, $sql);  
+  
+  $row = mysqli_fetch_array($rs_result);  
+  // $row = $rs_result;
+
+  // echo "<pre>";
+  // print_r($row);
+  // echo "</pre>";
+
+  $total_records = $row[0];  
+  $total_pages = ceil($total_records / $limit);  
+
+  $pagLink = "<div class='pagination'>";  
+  for ($i=1; $i<=$total_pages; $i++) {  
+             $pagLink .= " "."<a href='../html/homepage.php?page=".$i."'>".$i."</a>"."&nbsp;";  
+  };  
+  echo $pagLink . "</div>";  
+?>
+
+
 </body>
 </html>
--- php/product-catalog.php
+++ php/product-catalog.php
@@ -7,8 +7,23 @@
       header("Location: ../html/login.html");
     }
 
-    require_once("dbcontroller.php");
-	$db_handle = new DBController();
+    $servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "craigslist";
+	$limit = 20;
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+	else
+	{
+		echo "connection done";
+	}
 ?>
 
 <!DOCTYPE html>
@@ -17,38 +32,81 @@
 	<title>product catalog</title>
 	<link href="../css/cart.css" type="text/css" rel="stylesheet" />
 </head>
+
+
 <body>
  	<h2>this is the product catalog </h2>
 
  	<div id="product-grid"><!-- 
 	<div class="txt-heading">Utilities</div> -->
 	<?php
-	$utility_array = $db_handle->runQuery("SELECT * FROM utility ORDER BY utility_id");
-	
-	if (!empty($utility_array)) { 
-		foreach($utility_array as $key=>$value){
-		  $utility_image_array= $db_handle->runQuery("SELECT image_path FROM utility_image where utility_id=".$utility_array[$key]["utility_id"]);
-		  
-		// echo '<br><pre>'; 
-		//   print_r($utility_image_array);
-		//   print_r($utility_image_array[0]['image_path']);
-		// echo '</pre>';
-		?>
-		 <div class="product-item">
-			<form method="post" action="wishlist.php?action=add&utility_id=<?php echo $utility_array[$key]['utility_id']; ?>">
-			<div class="product-image"><img height="100" width="120" src="../php/<?php echo $utility_image_array[0]['image_path']; ?>"></div>
+
+	if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };  
+		$start_from = ($page-1) * $limit; 
+
+
+	// $utility_array = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM utility ORDER BY utility_id LIMIT $start_from, $limit;"));
+	$utility_array = array();
+	$result = mysqli_query($conn, "SELECT * FROM utility ORDER BY utility_id LIMIT $start_from, $limit;");
+
+	while($row = mysqli_fetch_assoc($result)){
+		// array_push($utility_array, $row);
+		$img_res =  mysqli_query($conn, "SELECT image_path FROM utility_image where utility_id=".$row["utility_id"]);
+		$path =	mysqli_fetch_assoc($img_res);
+
+		// echo "<pre>";
+		// print_r($row);
+		// print_r($path);
+		// echo "</pre>";
+?>
+		<div class="product-item">
+			<form method="post" action="wishlist.php?action=add&utility_id=<?php echo $row["utility_id"]; ?>">
+			<div class="product-image"><img height="100" width="120" src="../php/<?php echo $path["image_path"]; ?>"></div>
 			<div><strong><?php echo 
-			$utility_array[$key]["name"]; ?></strong></div>
-			<div class="product-price"><?php echo "$".$utility_array[$key]["price"]; ?></div>
+			$row["name"]; ?></strong></div>
+			<div class="product-price"><?php echo "$".$row["price"]; ?></div>
 			<div><input type="submit" value="Add to wishlist" class="btnAddAction" /></div>
 			<br>
 			</form>
 		</div>
-	
-	<?php
-		}
-	}
-	?>
+<?php	} ?>
+
+
+
 </div>
+
+<?php  
+	$sql = "SELECT COUNT(*) FROM utility;";  
+	$rs_result = mysqli_query($conn, $sql);  
+	
+	$row = mysqli_fetch_array($rs_result);  
+	// $row = $rs_result;
+
+	// echo "<pre>";
+	// print_r($row);
+	// echo "</pre>";
+
+	$total_records = $row[0];  
+	$total_pages = ceil($total_records / $limit);  
+
+	$pagLink = "<div class='pagination'>";  
+	for ($i=1; $i<=$total_pages; $i++) {  
+             $pagLink .= " "."<a href='../php/product-catalog.php?page=".$i."'>".$i."</a>"."&nbsp;";  
+	};  
+	echo $pagLink . "</div>";  
+?>
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
+
+
